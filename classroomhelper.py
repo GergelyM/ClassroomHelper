c0b13a851bef7b76374cf301c2d5a8cce0cbdeff
GergelyM <15053824@brookes.ac.uk>
2017-02-15 15:32:22 +0000

    added basic readme like info

--- classroomhelper.py
+++ classroomhelper.py
@@ -7,8 +7,11 @@ from dBhandler import *
 
 if __name__ == "__main__":
 
-    #print("Hello World")
-    genStdNum("John", "Smith", "02-14-1999")
-    # y
-    
-    
+    print("Hello World")
+
+    # this file shall contain all the core code created by David
+    # all the rest should be in seperate .py files in the project root folder
+    # to use them, simply import then without the .py extension
+    # just as the DBhandler module:
+    # from dBhandler import *
+
