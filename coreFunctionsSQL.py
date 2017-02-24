7601592eeb0100ba2eb102fd30df991329a44365
DavidMorp <maxidvdit@gmail.com>
2017-02-24 14:17:13 +0000

    Adaptations made to work with UI

--- coreFunctionsSQL.py
+++ coreFunctionsSQL.py
@@ -12,23 +12,40 @@ crDB = DbConn("db/crDB.db")
 c = crDB.cursor
 
 #Functions
+def getModules():
+    c.execute('SELECT moduleCode FROM module')
+    modules = c.fetchall()
+    i = 0 
+    for x in modules:
+        modules[i] = x[0]
+        i += 1
+    return modules
+
 def getModuleInfo(module):
     c.execute('SELECT * FROM module WHERE moduleCode = ?', [module]) #Get module deets
     line = c.fetchall()
     deets = line[0] #Select the frist and only tuple in the list
-    print(deets[1], deets[2], "\n" + deets[3]) #print selected data with spacing
+    moduleInfo = deets[1], deets[2], "\n" + deets[3]
+    #print(moduleInfo) #print selected data with spacing
+    return moduleInfo
 
 def displayGrades(module):
-    print("Getting grades for", module, "...")
-    print('%15s' % "Student ID",  '%15s' % "Name", '%13s' % "Surname", '%15s' % "DoB", '%10s' % "Grade") #Print header
+    title = "Getting grades for", module, "..."
+    #print("Getting grades for", module, "...")
+    #print('%15s' % "Student ID",  '%15s' % "Name", '%13s' % "Surname", '%15s' % "DoB", '%10s' % "Grade") #Print header
+    header = '%15s' % "Student ID",  '%15s' % "Name", '%13s' % "Surname", '%15s' % "DoB", '%10s' % "Grade"
     c.execute('SELECT * FROM grade WHERE moduleCode = ?', [module]) #Get grades
     allGrades = c.fetchall()
+    moduleList = []
     for grade in allGrades: #go through all the rows
         studentId = grade[3] #Find the student ID
         c.execute('SELECT * FROM student WHERE studentId = ?', [studentId]) #Get student deets
         line = c.fetchall()
         user = line[0] #Select the frist and only tuple in the list
-        print('%15s' % user[1], '%15s' % user[3], '%13s' % user[4], '%15s' % user[5], '%10s' % grade[0]) #print selected data with spacing
+        #print('%15s' % user[1], '%15s' % user[3], '%13s' % user[4], '%15s' % user[5], '%10s' % grade[0]) #print selected data with spacing
+        moduleItem = '%15s' % user[1], '%15s' % user[3], '%13s' % user[4], '%15s' % user[5], '%10s' % grade[0]
+        moduleList.append(moduleItem)
+    return (title, header, moduleList)
         
 def displayModules(teacher):
     c.execute('SELECT * FROM teacher WHERE teacherID = ?', [teacher]) #Get teacher info
@@ -68,8 +85,8 @@ def diplayMyGrades(student): #TODO David
 #TESTING:
 #getModuleInfo("M03010")
 #displayGrades("M03010")
-displayModules("st144228203")
-
+#displayModules("st144228203")
+#getModules()
 '''
 DO NOT REMOVE
 First attempt: DEPRECATED.
