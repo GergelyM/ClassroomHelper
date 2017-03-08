6d35cf8e2277b33a6731d94a2c655abd591bb96e
Salik <salik.dk@gmail.com>
2017-03-08 07:31:15 -0800

    GUI and comments added.

--- loginHandler.py
+++ loginHandler.py
@@ -1,12 +1,27 @@
+### Author: Salik, Gary
+### Trasferred from Pulled project file by Gary
+
+# it would be a bit more efficient if you would use a single data query for the user by login input name
+# in that way the code could decide if the input name exists in the DB,
+# and if it does, then that the stored password is equal to the user input
+# I will write the SQL statement here so you can just fit it in your code.
+# well practically I'll make a variable with the db field values above.
+
 from dbHandler import *
 from tkinter import *
 
+#################################################################################################
+# DO NOT CHANGE ANYTHING HERE, PLEASE!
+# IF YOU NEED SOME >CHANGE<, PLEASE LEAVE A COMMENT ABOVE. THANKS!!
+
 class LoginHandler:
+        # class-wide shared variables
         crDB = DbConn("db/crDB.db")
         c = crDB.cursor
 
         def __init__(self, master):
-
+            # python constructor method
+            # object-level variables, unique to every object
             self.master = master
             #Creates ID and password label s
             self.labelID = Label(master, text="ID").grid(row=0, sticky=E)
@@ -31,6 +46,7 @@ class LoginHandler:
             self.userPassword = self.EntryPassword.get()
 
         def fetchUserPw(self):
+            # this reads the object variables from __init__ method, to fetch data from DB
             newSq = ""
             try:
                 if self.userID[0:1].isnumeric():
@@ -41,25 +57,28 @@ class LoginHandler:
                 self.fetchedPw = self.c.fetchone()[0]
             except TypeError:
                 self.fetchedPw = False
+            # this returns the stored password belongs to the userID
             return self.fetchedPw
 
         def repeatInput(self):
+            # repeatedly asks for user input, until match, or number of tries used up, whichever happens first
             loopControl = True
+            # adding a counter here could be use to count/restrict failed attempts
             counter = 3
             returnVal = False
 
             while counter > 0 and loopControl != False:
                 self.readInput()
-
+                # self.fetchUserPw() can have 3 value: None (default), False (userId wrong)
+                # or the actual password if the UserID is valid (but the password is not, fails login though)
                 dbPassword = self.fetchUserPw()
                 if dbPassword == False:
                     if counter > 0:
                         self.successfulLogin = Label(self.master, text="Please try again.").grid(row=2, column=1)
                     else:
                         self.successfulLogin = Label(self.master, text="Login failed.").grid(row=2, column=1)
-
                     counter -= 1
-                elif dbPassword != False:
+                elif dbPassword != False: #may check for "" string with AND here also
                     if dbPassword == self.userPassword:
                         self.successfulLogin = Label(self.master, text="Authentication successful").grid(row=2, column = 1)
                         returnVal = self.userID
@@ -69,10 +88,22 @@ class LoginHandler:
                 else:
                     counter -= 1
             return returnVal
+            # tested for 3x wrong ID & wrong pass; 3x good teacher ID & wrong pass; 3x good studentId & wrong pass, returns False
+            # tested for 2x fail attempt + 1x good student Id & good pass, returns the ID
+            # tested for 2x fail attempt + 1x good teacher Id & good pass, returns the ID
 
         def login(self):
             return self.repeatInput()
 
+#################################################################################################
+#just comment out these lines if you need to work on this file, but leave them as is, thanks
+#st147707702
+#17159114703
+#p@ssword
+
 root = Tk()
 gui = LoginHandler(root)
 root.mainloop()
+
+
+#################################################################################################
