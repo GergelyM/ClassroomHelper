e10419da7b3e62566a384e830b6e2ed6103ead3a
GergelyM <15053824@brookes.ac.uk>
2017-03-12 00:50:03 +0000

    removed useless comments

--- dbHandler.py
+++ dbHandler.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python2
-#encoding: UTF-8
-
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-#FCK tinyDB, ditched for good, too complicated to be called "small and easy"
-
 import sqlite3
 
 class DbConn(object):
