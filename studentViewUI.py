6b011ceb236ff4498afef03b2c92b9569dc2f814
GergelyM <15053824@brookes.ac.uk>
2017-03-15 08:58:29 +0000

    multiple line of instructive comments added to the top of the file

--- studentViewUI.py
+++ studentViewUI.py
@@ -5,13 +5,63 @@ from dbHandler import *
 # To see it, run MainWindow with one of the student test IDs on line 13
 # Sorry it's messy - Connor
 
-# Test IDs  17153811308
-#           17151025709
-#           17145725904
-# Others do not work as no teacher for M03010
+# some new student IDs from DB v2 //Gary
+# Test IDs  1733675
+#           1734055
+#           1735936
+# Others do not work as no teacher for M03010 # should be ok in DB v2 have a look on db_README.txt // Gary
 
 # Made a few edits so it can be displayed in the main window
 
+
+############################################################
+#
+# to Connor:
+# I'm sorry, from yesterday the old DB structure won't be available
+# let's see how could you get the data for this view
+# at first let's word it:
+# you need the *grade(s)* for each *module* the of one single *student*
+#
+# by design data in *module* is just the description of the modules, it doesn't link to elsewhere but to *groupset* table
+#
+# these are the basic tables, with fields:
+# grade: id, grade, gradeType, studentID, groupsetID
+# groupset: id, groupsetName, year, semester, moduleCode, teacherID
+# module: id, moduleCode, moduleName, moduleDescription
+# student: id, studentID, studentGender, studentSurname, studentName, studentEmail, studentPassword
+#
+# so lets go through what we need as a sequence:
+# 1. we have the studentID, that's where we can start
+# 2. simple as is, we can search grade table for the studentID, and we get a bunch of data, good, but
+# we will not know which modules are those IDs for, neither the year/semester
+# 3. so we need to decide, either find all different groupsetIDs from the last search and build on that
+# or we can do a search on groupset table with the studentID and year at the same time, so we have a list of the students groups too
+# if you chose the second (I would) you can do the query on groupset first, and then use that data to search on grades
+# 4. anyway, we will verbose the modules, by getting their name from another query from module table,
+# that's simple, we have the moduleIDs from groupset query
+#
+# to sort this out, I would use multiple arrays for each query, would limit the fields to the ones we really need to work with/display
+# such as teacherID from groupset, to keep it simple, we don't care *as a student* who was the teacher (at least this stage)
+# or if you want a challenge, you can write a complex query by using standard SQL language using IN, EXIST, ORDER BY, and so on.
+# For example: http://stackoverflow.com/questions/4622453/where-col1-col2-in-sql-subquery-using-composite-primary-key
+#
+# (a little hint for 'order by') we do not store 'year' as DATE, it's just a simple TEXT field, it's sloppy but should be ok.
+# I'll change the DB once more if we get there ever.
+#
+# if you need multiple fields from a table, it could be done with a single DB query, like:
+# self.c.execute("SELECT * FROM student WHERE studentID = '" + self.userID + "'")
+# and then skim through it with a for loop, or
+# self.c.execute("SELECT studentName, studentSurname FROM student WHERE studentID = '" + self.userID + "'")
+# so you will get back the tuple with these two fields in a list
+#
+# I reckon it seems a bit much but one you start it will be kind of straight forward,
+# use a lot of prints to console to see if you get and pass the data you wanted, etc. but you know this anyway
+#
+# Give me a shout if you stuck somewhere
+# Keep up!
+# Gary
+#
+############################################################
 
 class Student(object):
 
@@ -19,9 +69,7 @@ class Student(object):
     c = crDB.cursor
 
     def __init__(self):
-
         self.userID = ""
-
 
     def setID(self, userID):
         self.userID = userID
